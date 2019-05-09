83374c27a2d023716c42831ecbf8d0f457f9317e
CarlTern <psy15cte@student.lu.se>
2019-05-09 14:47:46 +0200

    Update MainActivity.java

--- app/src/main/java/com/dynamicdusk/soundpocket/MainActivity.java
+++ app/src/main/java/com/dynamicdusk/soundpocket/MainActivity.java
@@ -1,23 +1,50 @@
 package com.dynamicdusk.soundpocket;
 
-import android.support.v4.content.WakefulBroadcastReceiver;
+import android.annotation.SuppressLint;
+
+import android.os.AsyncTask;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.webkit.*;
-
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import edu.cmu.pocketsphinx.Assets;
+import edu.cmu.pocketsphinx.Hypothesis;
+import edu.cmu.pocketsphinx.RecognitionListener;
+import edu.cmu.pocketsphinx.SpeechRecognizer;
+import edu.cmu.pocketsphinx.SpeechRecognizerSetup;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity implements
+        RecognitionListener {
+    /* We only need the keyphrase to start recognition, one menu with list of choices,
+       and one word that is required for method switchSearch - it will bring recognizer
+       back to listening for the keyphrase*/
+    private static final String KWS_SEARCH = "again";
+    private static final String MENU_SEARCH = "goodbye";
+    /* Keyword we are looking for to activate recognition */
+    private static final String KEYPHRASE = "activate";
+
+    /* Recognition object */
+    private SpeechRecognizer recognizer;
+
+
 
     SoundPlayer soundPlayer;
     MyJavaScriptInterface jsHandler;
     AccelerometerManager accelerometerManager;
+    WebView webView;
+
+
 
     private HashMap<String, AccelerometerListener> packages = new HashMap<String, AccelerometerListener>();
+
+
 
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -30,12 +57,14 @@ public class MainActivity extends AppCompatActivity{
         packages.put("Pistol", new Pistol());
 
         super.onCreate(savedInstanceState);
-        WebView webView = new WebView(this);
+        webView = new WebView(this);
         setContentView(webView);
         WebSettings settings = webView.getSettings();
         settings.setJavaScriptEnabled(true);
         settings.setLoadWithOverviewMode(true);
         settings.setUseWideViewPort(true);
+
+        runRecognizerSetup();
 
         settings.setBuiltInZoomControls(false);
         webView.setWebChromeClient(new WebChromeClient()); //making js alerts work
@@ -58,7 +87,7 @@ public class MainActivity extends AppCompatActivity{
         final WebView webViewCallbackAccess = webView;
         packages.get("Shotgun").setSoundPlayer(this.soundPlayer);
         if (AccelerometerManager.isSupported(this)) {
-            accelerometerManager.startListening(packages.get("Shotgun"));
+            AccelerometerManager.startListening(packages.get("Shotgun"));
         }
 
         Timer timer = new Timer();
@@ -72,17 +101,123 @@ public class MainActivity extends AppCompatActivity{
                 });
             }
         }, 3000);
+
     }
 
     public void setPackage(String key){
-        AccelerometerListener al = packages.get(key);
-        al.setSoundPlayer(this.soundPlayer);
+        packages.get(key).setSoundPlayer(this.soundPlayer);
         if (AccelerometerManager.isSupported(this)) {
-            accelerometerManager.startListening(packages.get(key));
-            float [] thresholds = al.getThresholds();
-            accelerometerManager.changeThreshold(thresholds[0], thresholds[1], thresholds[2]);
+            AccelerometerManager.startListening(packages.get(key));
         }
     }
+
+    @SuppressLint("StaticFieldLeak")
+    private void runRecognizerSetup() {
+        // Recognizer initialization is a time-consuming and it involves IO,
+        // so we execute it in async task
+        new AsyncTask<Void, Void, Exception>() {
+
+            protected Exception doInBackground(Void... params) {
+                try {
+                    Assets assets = new Assets(MainActivity.this);
+                    File assetDir = assets.syncAssets();
+                    setupRecognizer(assetDir);
+                } catch (IOException e) {
+                    return e;
+                }
+                return null;
+            }
+
+            protected void onPostExecute(Exception result) {
+                if (result != null) {
+                    System.out.println(result.getMessage());
+                } else {
+                    switchSearch(KWS_SEARCH);
+                }
+            }
+        }.execute();
+    }
+
+    private void setupRecognizer(File assetsDir) throws IOException {
+        recognizer = SpeechRecognizerSetup.defaultSetup()
+                .setAcousticModel(new File(assetsDir, "en-us-ptm"))
+                .setDictionary(new File(assetsDir, "cmudict-en-us.dict"))
+                // Disable this line if you don't want recognizer to save raw
+                // audio files to app's storage
+                //.setRawLogDir(assetsDir)
+                .getRecognizer();
+        recognizer.addListener(this);
+        // Create keyword-activation search.
+        recognizer.addKeyphraseSearch(KWS_SEARCH, KEYPHRASE);
+        // Create your custom grammar-based search
+        File menuGrammar = new File(assetsDir, "menu.gram");
+        recognizer.addGrammarSearch(MENU_SEARCH, menuGrammar);
+    }
+
+
+    public void onStop() {
+        super.onStop();
+        if (recognizer != null) {
+            recognizer.cancel();
+            recognizer.shutdown();
+        }
+    }
+
+
+    public void onPartialResult(Hypothesis hypothesis) {
+        if (hypothesis == null)
+            return;
+        String text = hypothesis.getHypstr();
+        if (text.equals(KEYPHRASE)) {
+            switchSearch(MENU_SEARCH);
+        }else if (text.equals("shotgun")) {
+            setPackage("Shotgun");
+        }else if (text.equals("mario")) {
+            setPackage("Mario");
+        }else if (text.equals("dab machine")) {
+            setPackage("MLG");
+        }else if (text.equals("warcraft")) {
+            setPackage("Warcraft3");
+        }else if (text.equals("pistol")) {
+            setPackage("Pistol");
+        } else if (text.equals("star wars")) {
+            setPackage("LightSaber");
+        }
+    }
+
+
+    public void onResult(Hypothesis hypothesis) {
+        if (hypothesis != null) {
+            System.out.println(hypothesis.getHypstr());
+        }
+    }
+
+
+    public void onBeginningOfSpeech() {
+    }
+
+
+    public void onEndOfSpeech() {
+        if (!recognizer.getSearchName().equals(KWS_SEARCH))
+            switchSearch(KWS_SEARCH);
+    }
+
+    private void switchSearch(String searchName) {
+        recognizer.stop();
+        if (searchName.equals(KWS_SEARCH))
+            recognizer.startListening(searchName);
+        else
+            recognizer.startListening(searchName, 10000);
+    }
+
+
+    public void onError(Exception error) {
+        System.out.println(error.getMessage());
+    }
+
+
+    public void onTimeout() {
+        switchSearch(KWS_SEARCH);
+    }
+
 }
-
-
